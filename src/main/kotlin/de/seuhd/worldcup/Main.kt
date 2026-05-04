f78c3489534f16d0ed1a34fffbf8a096d8d17616
Mai Kauod <mai.kauod@sap.com>
2026-05-04 10:21:32 +0200

    show standings function + initial menu

--- src/main/kotlin/de/seuhd/worldcup/Main.kt
+++ src/main/kotlin/de/seuhd/worldcup/Main.kt
@@ -4,20 +4,84 @@ import kotlinx.serialization.json.Json
 import java.io.File
 import kotlinx.serialization.decodeFromString
 
-
 fun main() {
     //TODO: Load JSON data
-    //TODO: Implement interactive menu
+    //TODO: Implement interactive men
     val json = File("world_cup_data.json").readText()
     val data = Json.decodeFromString<WorldCupData>(json)
-}
+
+
+    while(true) {
+        println("""
+    === FIFA World Cup 2026 ? Betting Console ===
+    1) Show Standings
+    2) Show Matches
+    3) Place Bets
+    4) Show Betting Score
+    5) Exit
+    =================================================
+    Choose an option (1 to 5) :""".trimMargin())
+
+
+    when (readln()){
+        "1" -> showStandings(data.groups)
+        "2" ->showMatches(data.groups)
+        "3" ->placeBets(data.groups)
+        "4" ->showBettingScore(data.groups)
+        "5" -> return
+        else -> println("invalid input")
+    }
+} }
 
 /* -------------------------------------------------------------
    1) Show Standings
    ------------------------------------------------------------- */
 private fun showStandings(allGroups: List<Group>) {
     //TODO
-}
+    println("select a group: ")
+    val groupselection = readln()
+
+        for (group in allGroups){
+            if (group.name != groupselection &&  groupselection.isNotBlank()){
+               continue
+            }
+            println("=== ${group.name}===")
+
+            val standings = mutableListOf<Triple<String, Int, Int>>()
+            for (team in group.teams){
+                var points = 0
+                var goalsF = 0
+                var goalsA = 0
+
+                for (match in group.matches) {
+                    if (match.homeScore == null || match.awayScore == null)
+                        continue
+
+                    if (team.id == match.homeTeam) {
+                        goalsF += match.homeScore
+                        goalsA += match.awayScore
+
+                        if (match.homeScore > match.awayScore) points += 3
+                        else if (match.homeScore == match.awayScore) points += 1
+                    }
+                }
+                val goaldif = goalsF - goalsA
+                standings.add(Triple(team.name, points, goaldif))
+                }
+            val sort = standings.sortedWith (
+                compareByDescending<Triple<String,Int,Int>> {it.second}.thenByDescending { it.third })
+            println("team - points - goal difference")
+            for (e in sort){
+                println("${e.first} ${e.second} ${e.third}")
+
+            }
+        }
+
+    }
+
+
+
+
 
 /* -------------------------------------------------------------
    2) Show Matches
@@ -37,5 +101,4 @@ private fun placeBets(allGroups: List<Group>) {
    4) Show Betting Score
    ------------------------------------------------------------- */
 private fun showBettingScore(allGroups: List<Group>) {
-    //TODO
-}
+//TODO
